349f0744f0781fc8a2afa4e8f840954a1ade834f
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-11-01 19:11:52 -0300

    feat login screen

---
+++ lib/components/auth/button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ButtonAuthComponent extends StatelessWidget {
+
+  final Function()? onTap;
+
+  const ButtonAuthComponent({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        padding: const EdgeInsets.all(15),
+        
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(
+          child: Text(
+            'Sign In',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
